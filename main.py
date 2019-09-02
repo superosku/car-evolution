3204b7ae6e2b2d5f97156e2aae5ceed479d85cfd
Oskari Lehto <oskari.lehto@gmail.com>
2019-09-02 09:01:42 +0300

    Tweaks

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ blue = pygame.color.Color(0, 0, 255)
 yellow = pygame.color.Color(0, 255, 255)
 
 
+car_start_pos = (5, 3)
+
+kill_count = 10
+total_car_count = 100
+mutate_factor = 0.1
+time_in_seconds = 30
+
+
 def vec_len(vec):
     return math.sqrt(vec[0] ** 2 + vec[1] ** 2)
 
@@ -39,8 +47,8 @@ def random_ground_points():
     ground_points = [(0, 0), (10, 0)]
     for i in range(200):
         ground_points.append((
-            ground_points[-1][0] + 1 + random.random() * 1.5,
-            ground_points[-1][1] + (random.random() - 0.49) * math.sqrt(i) * 0.5,
+            ground_points[-1][0] + 0.7 + random.random() * 1,
+            ground_points[-1][1] + (random.random() - 0.52) * math.sqrt(i) * 0.2,
         ))
     ground_points.insert(0, (
         ground_points[0][0],
@@ -58,27 +66,35 @@ def random_car_points():
         (
             random.random() * 2 - 1,
             random.random() * 2 - 1,
-            random.random() * 0.2 + 0.05,
-            i < 2
+            # random.random() * 0.2 + 0.05,
+            random.random() * 6 - 3, #  Used for sigmoid later
+            # i < random.randrange(1, 4),
+            i < 2,
             # random.random() < 0.5
         )
         for i in range(5)
     ]
+
     return car_pointss
 
 
 def mutate(car_points):
-    mutate_factor = 0.1
-    return [
+    car_points = [
         (
             x + (random.random() - 0.5) * mutate_factor,
             y + (random.random() - 0.5) * mutate_factor,
             radius + (random.random() - 0.5) * mutate_factor,
-            is_tire
+            is_tire if random.random() > 0.10 else not is_tire
         )
         for x, y, radius, is_tire
         in car_points
     ]
+    if random.random() < 0.10 and len(car_points) > 3:
+        random.shuffle(car_points)
+        car_points = car_points[0:-1]
+    if random.random() < 0.10 and len(car_points) < 7:
+        car_points = car_points + [random_car_points()[-1]]
+    return car_points
 
 
 class Simulation:
@@ -94,14 +110,13 @@ class Simulation:
 
         self.ground = self.world.CreateStaticBody()
         self.ground.CreateChainFixture(
-            vertices=ground_points
+            vertices=ground_points,
+            friction=0.7
         )
 
         self.car_points = car_points
         self.car_polygon_fixtures = []
         self.car_tires = []
-
-        car_start_pos = (5, 3)
 
         car_body = self.world.CreateDynamicBody(position=car_start_pos)
         self.car_body = car_body
@@ -153,13 +168,13 @@ class Simulation:
 
         self.car_polygon_fixtures.append(
             car_body.CreatePolygonFixture(
-                density=1,
-                friction=0.3,
+                density=10,
+                friction=0.7,
                 vertices=ordered_xys
             )
         )
 
-        for x, y, radius, is_motor in car_points:
+        for x, y, tire_stats, is_motor in car_points:
             if is_motor:
                 tire = self.world.CreateDynamicBody(
                     position=(
@@ -167,19 +182,27 @@ class Simulation:
                         car_start_pos[1] + y
                     ),
                 )
+
+                normalized_tire_stats = sigmoid(tire_stats)
+
+                radius = normalized_tire_stats * 0.5
+                friction = normalized_tire_stats
+                motor_speed = -70 * (1 - normalized_tire_stats)
+                motor_torque = 1000 * normalized_tire_stats
+
                 tire.CreateCircleFixture(
                     radius=radius,
-                    density=1,
-                    friction=0.7,
+                    density=10,
+                    friction=friction,
                     pos=(0, 0)
                 )
                 self.world.CreateRevoluteJoint(
                     bodyA=car_body,
                     bodyB=tire,
                     anchor=tire.position,
-                    motorSpeed=-40,
+                    motorSpeed=motor_speed,
                     enableMotor=True,
-                    maxMotorTorque=1000,
+                    maxMotorTorque=motor_torque,
                 )
                 self.car_tires.append(tire)
 
@@ -300,12 +323,15 @@ class Game:
 
         self.tick = 0
 
-        # self.car_pointss = []
-        self.car_pointss = [
-            random_car_points()
-            for _ in range(100)
-        ]
+        # self.car_pointss = [
+        #     random_car_points()
+        #     for _ in range(total_car_count)
+        # ]
+        self.car_pointss = [random_car_points()] * total_car_count
         self.simulations =[]
+
+        self.fast_mode = False
+        self.show_only_one = False
 
         self.init_simulations()
 
@@ -317,23 +343,28 @@ class Game:
             ) for car_points in self.car_pointss
         ]
 
+    def draw(self):
+        self.screen.fill(black)
+        self.simulations[0].draw_ground(self.screen, self.camera)
+        if self.show_only_one:
+            self.simulations[0].draw_car(self.screen, self.camera)
+        else:
+            for simulation in self.simulations:
+                simulation.draw_car(self.screen, self.camera)
+        pygame.display.flip()
+
     def simulation_tick(self):
         for simulation in self.simulations:
             if not pygame.key.get_pressed()[pygame.K_h]:
                 simulation.step()
 
-        sorted_simulations = sorted(self.simulations, key=lambda x: -x.score())
+        # sorted_simulations = sorted(self.simulations, key=lambda x: -x.score())
+        # simulation = sorted_simulations[0]
+        # simulation.update_camera(self.camera)
+        self.simulations[0].update_camera(self.camera)
 
-        simulation = sorted_simulations[0]
-        simulation.update_camera(self.camera)
-
-        self.screen.fill(black)
-        simulation.draw_ground(self.screen, self.camera)
-
-        if pygame.key.get_pressed()[pygame.K_f]:
-            for simulation in self.simulations:
-                simulation.draw_car(self.screen, self.camera)
-            pygame.display.flip()
+        if not self.fast_mode:
+            self.draw()
 
     def mutate(self):
         data = sorted([
@@ -344,25 +375,28 @@ class Game:
 
         print('best score:', data[0][1])
 
-        kill_count = 20
-
         surviving = [item for item, score in data[0:-kill_count]]
 
         for i in range(kill_count):
             surviving.append(
-                mutate(surviving[i])
+                mutate(
+                    surviving[i]
+                    if kill_count * 2 < total_car_count else
+                    surviving[0]
+                )
             )
 
         self.car_pointss = surviving
+
         self.init_simulations()
 
     def simulate(self):
+        round_number = 1
         while True:
-
             self.clock.tick()
 
             try:
-                pygame.display.set_caption(f'UPS: {int(self.clock.get_fps())}')
+                pygame.display.set_caption(f'UPS: {int(self.clock.get_fps())} round: {round_number}')
             except OverflowError:
                 pass
 
@@ -374,8 +408,10 @@ class Game:
                         return
                     if event.key == pygame.K_r:
                         self.init_simulations()
-                if event.type == pygame.MOUSEWHEEL:
-                    breakpoint()
+                    if event.key == pygame.K_f:
+                        self.fast_mode = not self.fast_mode
+                    if event.key == pygame.K_o:
+                        self.show_only_one = not self.show_only_one
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 4:
                         self.camera.zoom *= 1.1
@@ -386,9 +422,11 @@ class Game:
 
             self.tick += 1
 
-            if self.tick % (60 * 20) == 0:
+            if self.tick % (60 * time_in_seconds) == 0:
+                if self.fast_mode:
+                    self.draw()
                 self.mutate()
-
+                round_number += 1
 
 
 if __name__ == '__main__':
